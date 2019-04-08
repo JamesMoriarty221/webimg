2ac8e84244d4fdcb296179bb3508437ceffd2669
Charles <g_lemarc@yahoo.com>
2019-04-08 00:23:19 -0400

    Revert "adding google engine"
    This reverts commit c58e4b31accfcae992b6cc66d7c77b8e1a924e2a.

--- test_googleim.py
+++
@@ -1,9 +0,0 @@
-from google_images_download import google_images_download   #importing the library
-
-response = google_images_download.googleimagesdownload()   #class instantiation
-
-#arguments = {"keywords":"Polar bears","limit":20,"print_urls":True,"no_download":""}   #creating list of arguments
-arguments = {"keywords":"pussy creampie","no_download":"no_download", "limit":10}   #creating list of arguments
-paths,img_list = response.download(arguments)   #passing the arguments to the function
-print(paths)   #printing absolute paths ofcccccccccccccccc the downloaded images
-print(img_list)
